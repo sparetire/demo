dc5e60f03711cac0ee5fd133c9a8c25ad8e7b1d6
Sparetire <s9ar371r3@gmail.com>
2016-10-11 16:50:41 +0800

    add demo 13 14

---
+++ demo13/demo.js
@@ -0,0 +1,10 @@
+$(function () {
+	var mySwiper = new Swiper('.swiper-container', {
+		direction: 'vertical',
+		loop: true,
+		// pagination: '.swiper-pagination',
+		// nextButton: '.swiper-button-next',
+		// prevButton: '.swiper-button-prev',
+		// scrollbar: '.swiper-scrollbar'
+	});
+});
